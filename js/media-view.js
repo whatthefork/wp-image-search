7738b7c589f9045500652f2113227c183a0d377f
cristian-ungureanu <cristian@themeisle.com>
2014-12-02 11:14:05 +0200

    small changes

--- js/media-view.js
+++ js/media-view.js
@@ -5,7 +5,8 @@
 
 
 	routerView = media.view.routerView = typeof _wpMediaViewsL10n === 'undefined' ? {} : _wpMediaViewsL10n;
-	// override media input methods.
+
+	/*Tabs of Media Library*/
 	media.view.MediaFrame.Select.prototype.browseRouter = function( view ) {
 		var set_data = {};
 		set_data.upload = {
@@ -24,57 +25,41 @@
 		};
 		view.set(set_data);
 	};
+
 	
+	/*Content of GSearch tab*/
 	wp.media.view.Router = wp.media.view.Router.extend(
 	{
 		select : function(id){
-		  var view = this.get( id );
-		  this.deselect();
-		  if(view && view.$el)
-			view.$el.addClass('active');
+			var view = this.get( id );
+			this.deselect();
+			if(view && view.$el)
+				view.$el.addClass('active');
 	 
-		 if(id == "gsearch"){
-			
-			
-			
-			jQuery(".media-frame-content").html("<div class='ginputs'><form id='gsearch_form' action='"+path_var.path+"' method='POST'><input type='text' name='search_input' placeholder='Search' class='search gsearch_text_input'/><input type='submit' id='gsearch_submit' class='button gsearch_input_button'/></div>");
-
-			
-			//do something
-			jQuery("#gsearch_form").submit(function(e){
-				var postData = jQuery(this).serializeArray();
-				var formURL = jQuery(this).attr("action");
-				
-				jQuery.ajax(
-				{
-					url : formURL,
-					type: "POST",
-					data : postData,
-					success:function(data, textStatus, jqXHR) 
+			 if(id == "gsearch"){
+				jQuery(".media-frame-content").html("<div class='ginputs'><form id='gsearch_form' action='"+path_var.path+"' method='POST'><input type='text' name='search_input' placeholder='Search' class='search gsearch_text_input'/><input type='submit' id='gsearch_submit' class='button gsearch_input_button'/></div>");
+				jQuery("#gsearch_form").submit(function(e){
+					var postData = jQuery(this).serializeArray();
+					var formURL = jQuery(this).attr("action");
+					
+					jQuery.ajax(
 					{
-						alert(data);
-						//data: return data from server
-					},
-					error: function(jqXHR, textStatus, errorThrown) 
-					{
-						//if fails      
-					}
+						url : formURL,
+						type: "POST",
+						data : postData,
+						success:function(data, textStatus, jqXHR) 
+						{
+							alert(data);
+						},
+						error: function(jqXHR, textStatus, errorThrown) 
+						{
+							//if fails      
+						}
+					});
+					e.preventDefault(); 
 				});
-				e.preventDefault(); //STOP default action
-			});
- 
-
-			
-			
-			
-						
-			
-			//probably there is a better way
+			}
 		}
-    }
-});
-
+	});
 	
-	
-
 })(jQuery,window);
